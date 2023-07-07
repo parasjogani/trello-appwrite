864155b5de470466bc60eb3aa48f2699fe4c977a
parasjogani <paras.jogani@gmail.com>
2023-07-07 17:48:28 +0530

    header complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Board from "@/components/Board";
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello Trello</h1>
+    <main>
+      {/* Header */}
+      <Header />
+      
+      {/* Body */}
+      <Board />
     </main>
   )
 }
