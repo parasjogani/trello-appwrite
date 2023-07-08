80be2269885e035d5b79b9ac72b53d04bbeafc1d
parasjogani <paras.jogani@gmail.com>
2023-07-08 11:15:36 +0530

    search function

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,4 +1,6 @@
+import { useBoardStore } from "@/store/BoardStore";
 import { PlusCircleIcon } from "@heroicons/react/24/solid";
+import { todo } from "node:test";
 import { Draggable, Droppable } from "react-beautiful-dnd"
 import TodoCard from "./TodoCard";
 type Props = {
@@ -16,6 +18,8 @@ const idToColumnText: {
 };
 
 function Column({ id, todos, index }: Props) {
+    const [searchString] = useBoardStore((state) => [state.searchString])
+
     return (
         <Draggable draggableId={id} index={index}>
             {(provided) => (
@@ -32,28 +36,35 @@ function Column({ id, todos, index }: Props) {
                                 className={`p-2 rounded-2xl shadow-sm ${snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"}`}
                             >
                                 <h2 className="flex justify-between font-bold text-xl p-2">{idToColumnText[id]}
-                                    <span className="text-gray-500 bg-gray-200 rounded-full px-2 py-1 text-sm font-normal">{todos.length}</span>
+                                    <span className="text-gray-500 bg-gray-200 rounded-full px-2 py-1 text-sm font-normal">{
+                                        !searchString ?
+                                            todos.length : todos.filter(todo => todo.title.toLowerCase().includes(searchString.toLowerCase())).length}</span>
                                 </h2>
 
                                 <div className="space-y-2">
-                                    {todos.map((todo, index) => (
-                                        <Draggable
-                                            key={todo.$id}
-                                            draggableId={todo.$id}
-                                            index={index}
-                                        >
-                                            {(provided) => (
-                                                <TodoCard
-                                                    todo={todo}
-                                                    index={index}
-                                                    id={id}
-                                                    innerRef={provided.innerRef}
-                                                    draggableProps={provided.draggableProps}
-                                                    dragHandleProps={provided.dragHandleProps}
-                                                />
-                                            )}
-                                        </Draggable>
-                                    ))}
+                                    {todos.map((todo, index) => {
+                                        if (searchString && !todo.title.toLowerCase().includes(searchString.toLowerCase()))
+                                            return null;
+
+                                        return (
+                                            <Draggable
+                                                key={todo.$id}
+                                                draggableId={todo.$id}
+                                                index={index}
+                                            >
+                                                {(provided) => (
+                                                    <TodoCard
+                                                        todo={todo}
+                                                        index={index}
+                                                        id={id}
+                                                        innerRef={provided.innerRef}
+                                                        draggableProps={provided.draggableProps}
+                                                        dragHandleProps={provided.dragHandleProps}
+                                                    />
+                                                )}
+                                            </Draggable>
+                                        )
+                                    })}
                                     {provided.placeholder}
                                     <div className="flex items-end justify-end p-2">
                                         <button className="text-green-500 hover:text-green-600">
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,21 @@
 import Image from "next/image"
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid"
 import Avatar from "react-avatar"
+import { useBoardStore } from "@/store/BoardStore"
+import { stat } from "fs"
 
 function Header() {
+    const [searchString, setSearchString] = useBoardStore((state) => [
+        state.searchString,
+        state.setSearchString,
+    ])
     return (
         <header>
             <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
-            <div
-                className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055D1] rounded-md filter blur-3xl opacity-50 -z-50"
-                
-            />
+                <div
+                    className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055D1] rounded-md filter blur-3xl opacity-50 -z-50"
+
+                />
                 <Image
                     src={"https://links.papareact.com/c2cdd5"}
                     alt="trello logo"
@@ -23,7 +29,10 @@ function Header() {
                 <div className="flex items-center space-x-5 flex-1 justify-end w-full">
                     <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
                         <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
-                        <input type="text" placeholder="Search" className="flex-1 outline-none p-2" />
+                        <input type="text" placeholder="Search"
+                            value={searchString}
+                            onChange={e => setSearchString(e.target.value)}
+                            className="flex-1 outline-none p-2" />
                         <button hidden>Search</button>
                     </form>
 
